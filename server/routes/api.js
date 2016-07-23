dd6fd4e1f35f88e051059a0d783d98169dafc883
RexCorpDev <rexcorpfx@gmail.com>
2016-07-23 14:27:46 -0700

    refactoring user favorites.

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,5 +4,6 @@ const router = new express.Router();
 router.use('/crud', require('./cruds'));
 router.use('/users', require('./users'));
 router.use('/yelp', require('./yelp'));
+router.use('/oauth', require('./oauth'));
 
 module.exports = router;
